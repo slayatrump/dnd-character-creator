47de49a4de4761b00b47b9ab02bee9cae0ef2062
ncowden4 <71358000+ncowden4@users.noreply.github.com>
2021-02-15 02:51:56 -0500

    More Scenes Add + 3 MainMenu Mockups

---
+++ DnDCC/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    GameObject mStrips;
+    GameObject oStrips;
+
+    private bool isOptionsActive;
+
+    public void Start()
+    {
+
+
+        mStrips = GameObject.Find("MMStrips");
+        oStrips = GameObject.Find("OptionsStrips");
+
+        oStrips.SetActive(false);
+        isOptionsActive = false;
+    }
+
+    public void NewCharacter()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void LoadCharacter()
+    {
+        SceneManager.LoadScene(14);
+    }
+
+    public void Options()
+    {
+        if (isOptionsActive == true)
+        {
+            mStrips.SetActive(true);
+            oStrips.SetActive(false);
+            isOptionsActive = false;
+        }
+        else
+        {
+            mStrips.SetActive(false);
+            oStrips.SetActive(true);
+            isOptionsActive = true;
+        }
+    }
+
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+}
