466b2b626fa796664cda8d203c62fa08895617a5
ncowden4 <71358000+ncowden4@users.noreply.github.com>
2021-02-22 21:40:43 -0500

    CSV Reader WIP

---
+++ DnDCC/Assets/Scripts/CSVParsing.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class CSVParsing : MonoBehaviour
+{
+    public TextAsset csvFile; // Reference of CSV file
+    public Text contentArea; // Reference of contentArea where records are displayed
+
+
+    private char lineSeperater = '\n'; // It defines line seperate character
+    private char fieldSeperator = ','; // It defines field seperate chracter
+
+    void Start()
+    {
+        readData();
+    }
+
+    // Read data from CSV file
+    private void readData()
+    {
+        string[] records = csvFile.text.Split(lineSeperater);
+        foreach (string record in records)
+        {
+            string[] fields = record.Split(fieldSeperator);
+            foreach (string field in fields)
+            {
+                contentArea.text += field + "\t";
+            }
+            contentArea.text += '\n';
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
