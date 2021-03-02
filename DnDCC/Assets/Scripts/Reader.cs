da55a2bd7ff58ddf2c1cb4b508c25cddde5cb63d
ncowden4 <71358000+ncowden4@users.noreply.github.com>
2021-03-01 23:01:31 -0500

    More CSV Testing

---
+++ DnDCC/Assets/Scripts/Reader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reader : MonoBehaviour
+{
+    public Text txt;
+    string ext;
+
+    public void Awake()
+    {
+        List<Dictionary<string, object>> data = CSVReader.Read("Test");
+
+        for (int i = 0; i < data.Count; i++)
+        {
+            ext += ("Name " + data[i]["Name"] + " " +
+                "Number " + data[i]["Number"] + " ");
+        }
+
+        txt.text = ext;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
