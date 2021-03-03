b31ebd0d7ad86c8f831887e0ae50515371ffadd6
ncowden4 <71358000+ncowden4@users.noreply.github.com>
2021-03-03 16:53:52 -0500

    Button Event System Started

---
+++ DnDCC/Assets/Scripts/SelectionController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SelectionController : MonoBehaviour
+{
+    public Button b1;
+    public Button b2;
+    public Button b3;
+
+    string selected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        b1.GetComponent<Image>().color = Color.green;
+        b2.GetComponent<Image>().color = Color.green;
+        b3.GetComponent<Image>().color = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //b1.onClick.AddListener(Selected);
+    }
+
+    public void Selected()
+    {
+        selected = EventSystem.current.currentSelectedGameObject.name;
+
+        //if (b1)
+        //{
+        //    b1.GetComponent<Image>().color = Color.green;
+        //    b2.GetComponent<Image>().color = Color.green;
+        //    b3.GetComponent<Image>().color = Color.green;
+        //}
+
+        switch (selected)
+        {
+            case "BarbarianSelect":
+                {
+                    b1.GetComponent<Image>().color = Color.white;
+                    b2.GetComponent<Image>().color = Color.green;
+                    b3.GetComponent<Image>().color = Color.green;
+                    break;
+                }
+            case "BardSelect":
+                {
+                    b1.GetComponent<Image>().color = Color.green;
+                    b2.GetComponent<Image>().color = Color.white;
+                    b3.GetComponent<Image>().color = Color.green;
+                    break;
+                }
+            case "ClericSelect":
+                {
+                    b1.GetComponent<Image>().color = Color.green;
+                    b2.GetComponent<Image>().color = Color.green;
+                    b3.GetComponent<Image>().color = Color.white;
+                    break;
+                }
+        }
+
+
+    }
+}
