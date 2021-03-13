218a5102e623a25143b6bd37cef0d2af5b84a13b
Tyler Trumpie <tylertrumpie@gmail.com>
2021-03-13 15:08:47 -0500

    Race prefabs created
    -Race class created
    -All races and variants created and put into prefabs

---
+++ DnDCC/Assets/Scripts/Race.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Race : MonoBehaviour
+{
+    public enum ASType { Str, Dex, Con, Int, Wis, Cha, All, ChoicePool};
+    public enum SizeType { Small, Medium, Large };
+
+    [System.Serializable]
+    public class ASIncrease
+    {
+        public ASType AbilityScore;
+        public int IncreaseBy;
+    }
+
+    public ASIncrease AS1;
+    public ASIncrease AS2;
+
+    public SizeType Size;
+
+    public int Speed;
+
+    public string[] Languages;
+
+    public string[] Features;
+}
