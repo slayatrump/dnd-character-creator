9baaf11d66fa964b5786930e40da241776c16e8f
Tyler Trumpie <tylertrumpie@gmail.com>
2021-03-28 17:46:34 -0400

    Weapon and Spell scripts added
    -Weapon script created
    -Spell script created
    -1 prefab for weapon and spell created
    -Updated GlobalEnums
    -Had weird issues with merging but fixed them up

---
+++ DnDCC/Assets/Scripts/Objects/Spell.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GlobalEnums;
+
+public class Spell : MonoBehaviour
+{
+    public string SpellName;
+
+    public int SpellLevel;
+
+    public SchoolTypes SchoolType;
+
+    public string CastingTime;
+
+    public string Range;
+
+    public string Components;
+
+    public string Duration;
+
+    //Putting this in its own class structure allows class level to be grabbed individually without touching the class name if it is ever needed
+    [System.Serializable]
+    public class ClassPrerequisite
+    {
+        public ClassTypes Class;
+        public int ClassLevel;
+    }
+
+    public List<ClassPrerequisite> Classes;
+
+    //Using a list here to split descriptions by sections, such as if it has bullet points like for prestidigitation
+    public List<string> DescriptionParagraphs;
+
+
+}
---
+++ DnDCC/Assets/Scripts/Objects/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GlobalEnums;
+
+public class Weapon : MonoBehaviour
+{
+    //https://roll20.net/compendium/dnd5e/Weapons#content This has all the weapons listed
+
+    public string WeaponName;
+
+    public enum WeaponTypes { SimpleMelee, SimpleRanged, MartialMelee, MartialRanged};
+    public WeaponTypes WeaponType;
+
+    //Numcost is the number, Costtype is gp, sp, cp, etc.
+    public int NumCost;
+    public string CostType;
+
+    //1d4, 2d6, 1d8, etc.
+    public string Damage;
+    public DamageTypes DamageType;
+
+    public float Weight;
+
+    public List<string> Properties;
+}
