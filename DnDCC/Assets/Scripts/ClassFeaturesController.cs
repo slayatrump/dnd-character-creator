9167b02cd82d687e72cc64534748b68c1ef4619f
Tyler Trumpie <tylertrumpie@gmail.com>
2021-04-12 23:51:04 -0400

    ClassFeatures populated and selectable
    -ClassFeatures scene has been populated
    -Code checks the instance racename, if it is cleric, fighter, sorcerer, or warlock, the linked panel enables itself, otherwise it will automatically move to the spells scene
    -Still need to make it so you can choose 2 for cleric domains

---
+++ DnDCC/Assets/Scripts/ClassFeaturesController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClassFeaturesController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //TEST CODE REMOVE BEFORE DELIVERABLE
+        SaveManager.instance.gameData.raceName = "Warlock";
+        //END TEST CODE
+
+        if (SaveManager.instance.gameData.raceName == "Warlock")
+        {
+            //enable the warlock panel
+            transform.GetChild(3).gameObject.SetActive(true);
+
+        }
+        else if(SaveManager.instance.gameData.raceName == "Sorcerer")
+        {
+            //enable the sorcerer panel
+            transform.GetChild(2).gameObject.SetActive(true);
+        }
+        else if(SaveManager.instance.gameData.raceName == "Fighter")
+        {
+            //enable the fighter panel
+            transform.GetChild(1).gameObject.SetActive(true);
+        }
+        else if(SaveManager.instance.gameData.raceName == "Cleric")
+        {
+            //enable the cleric panel
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+        else
+        {
+            //go to next scene (spells)
+            Debug.Log("Class Features skipped due to class not having a feature to choose");
+            SceneManager.LoadScene("Spells");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
