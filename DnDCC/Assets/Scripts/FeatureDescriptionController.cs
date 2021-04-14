c87815feda9368a7e78726d443b1c4334f790b23
Tyler Trumpie <tylertrumpie@gmail.com>
2021-04-14 15:35:23 -0400

    pushing stashed changes
    2nd half of the push
    -descriptions now appear when selecting an option

---
+++ DnDCC/Assets/Scripts/FeatureDescriptionController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FeatureDescriptionController : MonoBehaviour
+{
+    Text desc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        desc = transform.GetComponentInChildren<Text>();
+        desc.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeDesc(string text)
+    {
+        desc.text = text;
+    }
+
+}
