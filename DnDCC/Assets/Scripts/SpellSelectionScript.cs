041f25c2039752397610512ee8a00a3034235494
Tyler Flores <63425639+Pre0rdered@users.noreply.github.com>
2021-04-17 15:05:44 -0400

    Tyler Flores - Spell Selection Scene Work
    Did some redesign on the spell selection scene and did a chunk of coding. Unfinished though, need help.

--- DnDCC/Assets/Scripts/SpellSelectionScript.cs
+++ DnDCC/Assets/Scripts/SpellSelectionScript.cs
@@ -19,6 +19,14 @@ public class SpellSelectionScript : MonoBehaviour
 
     GameObject[] displayThese; //the cantrips to display
 
+    //Two arrays for each section of components and descriptions of the spells
+    string[] spellComponentsArray;
+    string[] spellDescriptionArray;
+
+    //This is the spell that is chosen when the user hits the checkbox
+    string selectedSpellText;
+    GameObject selectedSpell;
+
     //Test case at the moment
     //When functional, it should just take the class name from the current save data and use that instead
     public string chosenClass = "Bard"; //SaveManager.instance.gameData.className;
@@ -33,7 +41,7 @@ public class SpellSelectionScript : MonoBehaviour
 
         FindCantrips(chosenClass);
         FindFirstLevel(chosenClass);
-        
+
         //TODO: Get population to work right
         PopCantripList();
         PopFirstLevelList();
@@ -43,12 +51,14 @@ public class SpellSelectionScript : MonoBehaviour
         GameObject.Find("1L Number").GetComponent<TextMeshProUGUI>().text = $"{numList[1]}";
     }
 
+    //Methods to set the Cantrip and First Level spell numbers according to chosen class
     #region Setting Spell Numbers
     public int[] SetNumbers(string chosenClass)
     {
         //array can be increased to 3 if spell slots are needed
         int[] numList = new int[2];
 
+        //Only classes that have spellcasting at the start are shown
         switch (chosenClass)
         {
             case "Bard":
@@ -93,6 +103,7 @@ public class SpellSelectionScript : MonoBehaviour
     }
     #endregion
 
+    //Methods to grab the appropriate cantrips for the class and then populate the scroll list with them
     #region Cantrips Methods
     public GameObject[] FindCantrips(string chosenClass)
     {
@@ -117,7 +128,7 @@ public class SpellSelectionScript : MonoBehaviour
         return displayThese;
     }
 
-    //TODO: Something needs to be set to an instance of an object here,
+    //TODO: Something needs to be set to an instance of an object here, but I can't figure out what
     public void PopCantripList()
     {
         //for each element that passed the check, create a checkbox for it
@@ -127,6 +138,8 @@ public class SpellSelectionScript : MonoBehaviour
             GameObject cantripCheckbox = Instantiate(checkboxPrefab);
 
             //checkbox needs to be a parent of the right scrollpanel so it populates properly
+            //The scroll panel object in the project has components that will organize things properly, so the only thing that has to be done
+            //is setting the new checkbox as a parent of the right panel, no need for more transofrmation edits past that.
             cantripCheckbox.transform.parent = GameObject.Find("CantripPanel/CantripList/Scroll/ScrollPanel").transform;
 
             //The text of the textbox should be the text of the spell name
@@ -135,6 +148,8 @@ public class SpellSelectionScript : MonoBehaviour
     }
     #endregion
 
+    //TODO: Once the Cantrips methods are solved, copy past the information into the First Level methods and replace names appropriately
+    //Methods to grab the appropriate first level spells for the class and then populate the scroll list with them
     #region First Level Spells Methods
     public GameObject[] FindFirstLevel(string chosenClass)
     {
@@ -172,6 +187,54 @@ public class SpellSelectionScript : MonoBehaviour
 
             //The text of the textbox should be the text of the spell name
             cantripCheckbox.GetComponentInChildren<Text>().text = displayThese[i].name;
+        }
+    }
+    #endregion
+
+    //Methods to display the spell information when the user selects a spell
+    #region Spell Information Methods
+    //This method should be called when the checkbox is checked or unchecked.
+    //There is an onvaluechanged feature with the unity checkbox, but I am unsure how to get it to work with this
+    public void SpellInformation(GameObject[] displayThese)
+    {
+        //the string selectedSpell needs to be set based on text of the checkbox that was just clicked
+        //the following code isn't functional
+
+        //selectedSpellText is the item used to compare
+
+        for (int i = 0; i < displayThese.Length; i++)
+        {
+            //if both texts match up, make the GameObject selectedSpell equal displayThese[i]
+            if (selectedSpellText == displayThese[i].GetComponent<Text>().text)
+            {
+                selectedSpell = displayThese[i];
+            }
+        }
+
+        //Sets the text of each respective object in the scene to the text of the respective property
+        GameObject.Find("NText").GetComponent<Text>().text = 
+            selectedSpell.GetComponent<Spell>().SpellName.ToString();
+        GameObject.Find("SText").GetComponent<Text>().text = 
+            selectedSpell.GetComponent<Spell>().SchoolType.ToString();
+        GameObject.Find("CTText").GetComponent<Text>().text = 
+            selectedSpell.GetComponent<Spell>().CastingTime.ToString();
+        GameObject.Find("RText").GetComponent<Text>().text = 
+            selectedSpell.GetComponent<Spell>().Range.ToString();
+
+        //Should go through the list of items in components and get then display the text properly
+        for (int i = 0; i < selectedSpell.GetComponent<Spell>().Components.Count; i++)
+        {
+            spellComponentsArray[i] = selectedSpell.GetComponent<Spell>().Components[i].ToString();
+            GameObject.Find("CompText").GetComponent<Text>().text =
+                string.Join(spellComponentsArray[i], "/n");
+        }
+
+        //same as above but for the descriptions instead
+        for (int i = 0; i < selectedSpell.GetComponent<Spell>().DescriptionParagraphs.Count; i++)
+        {
+            spellDescriptionArray[i] = selectedSpell.GetComponent<Spell>().DescriptionParagraphs[i];
+            GameObject.Find("DText").GetComponent<Text>().text =
+                string.Join(spellDescriptionArray[i], "/n");
         }
     }
     #endregion
